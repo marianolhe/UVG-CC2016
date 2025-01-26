8bfce3f6934978f5b62a5fbaf9ff73f49a41a71f
marianolhe <168101818+marianolhe@users.noreply.github.com>
2025-01-25 19:45:05 -0600

    interfaz con comentarios ¿

--- blender-project/src/main/java/uvg/edu/com/blenderInterface.java
+++ blender-project/src/main/java/uvg/edu/com/blenderInterface.java
@@ -1,5 +1,21 @@
 package uvg.edu.com;
+/**
+ * Interfaz realizada para comprobar funcionamiento con otros grupos
+ * @author toda la clase
+ * @version 1.0
+ */
 
-public class blenderInterface {
-    
-}
+public interface Licuadora {
+    public interface Licuadora {
+        void encender();
+        void apagar();
+        boolean estaEncendida();
+        double llenar(double volumen);
+        int incrementarVelocidad();
+        int decrementarVelocidad();
+        int consultarVelocidad();
+        boolean estaLlena();
+        double vaciar();
+        double servir(double volumenRestado);
+        }}
+            
