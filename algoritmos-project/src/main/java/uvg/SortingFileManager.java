f47086ac0c9aae06c3bb12ce49c10f15f1a9043d
anaxmarcela <marcelaoe55@gmail.com>
2025-02-10 11:40:50 -0600

    Creación clase SortingFileManager

--- algoritmos-project/src/main/java/uvg/SortingFileManager.java
+++ algoritmos-project/src/main/java/uvg/SortingFileManager.java
@@ -1,5 +1,29 @@
 package uvg;
 
-public class SortingFileManager {
-    x
+class SortingFileManager {
+    private final String filename;
+
+    public SortingFileManager(String filename) {
+        this.filename = filename;
+    }
+
+    public void writeToFile(int[] data) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (int num : data) {
+                writer.write(num + "\n");
+            }
+        }
+    }
+
+    public int[] readFromFile() throws IOException {
+        List<Integer> numbers = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+        }
+        return numbers.stream().mapToInt(i -> i).toArray();
+    }
 }
+
