4b10ffc1eab71a300fbda25fd31f007996b8a659
anaxmarcela <marcelaoe55@gmail.com>
2025-02-28 21:10:59 -0600

    cerras scanner

--- infixtopostfix/src/main/java/uvg/StackFactory.java
+++ infixtopostfix/src/main/java/uvg/StackFactory.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 public class StackFactory {
     public static <T> IStack<T> createStack() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Seleccione la implementacion de la pila:");
+        System.out.println("Seleccione la implementación de la pila:");
+
         System.out.println("1. ArrayListStack");
         System.out.println("2. VectorStack");
         System.out.println("3. ListStack (Lista Enlazada)");
 
         int choice = scanner.nextInt();
+        scanner.close();
         
         switch (choice) {
             case 1: return new ArrayListStack<>();
             case 2: return new VectorStack<>();
             case 3: return new ListStack<>();
             default:
-                System.out.println("Opcion no valida. Se usara VectorStack por defecto.");
+                System.out.println("Opción no válida. Se usará VectorStack por defecto.");
+
                 return new VectorStack<>();
             }
         }
