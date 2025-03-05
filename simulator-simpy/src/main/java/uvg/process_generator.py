709250f223ede62d0423aad54788e3146b73c0ba
anaxmarcela <marcelaoe55@gmail.com>
2025-03-04 19:36:22 -0600

    process_generator y cambio del nombre del main

---
+++ simulator-simpy/src/main/java/uvg/process_generator.py
@@ -0,0 +1,28 @@
+import simpy
+import random
+from process import Process
+
+class ProcessGenerator:
+    def _init_(self, env, memory, cpu, interval, start_times, end_times, num_processes, cpu_quantum):
+        """
+        Inicializa el generador de procesos con un número máximo de procesos.
+        """
+        self.env = env
+        self.memory = memory
+        self.cpu = cpu
+        self.interval = interval
+        self.process_count = 0
+        self.start_times = start_times
+        self.end_times = end_times
+        self.num_processes = num_processes
+        self.cpu_quantum = cpu_quantum
+        self.action = env.process(self.generate())
+
+        def generate(self):
+        
+        # Genera nuevos procesos en intervalos de tiempo exponenciales hasta alcanzar el límite.
+        
+        while self.process_count < self.num_processes:
+            yield self.env.timeout(random.expovariate(1.0 / self.interval))
+            Process(self.env, self.process_count, self.memory, self.cpu, self.start_times, self.end_times, self.cpu_quantum)
+            self.process_count += 1
