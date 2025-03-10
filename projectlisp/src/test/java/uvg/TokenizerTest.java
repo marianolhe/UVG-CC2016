82049ae4d2c4e87ad48c6dbd40515c38d69f5e98
marianolhe <168101818+marianolhe@users.noreply.github.com>
2025-03-10 12:09:11 -0600

    test para palabras clave

--- projectlisp/src/test/java/uvg/TokenizerTest.java
+++ projectlisp/src/test/java/uvg/TokenizerTest.java
@@ -5,5 +5,22 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TokenizerTest {
+    @Test
+    
+void testTokenizeKeywords() {
+    Tokenizer tokenizer = new Tokenizer("defun cond setq");
+    List<Token> tokens = tokenizer.Tokenize();
+
+    assertEquals(3, tokens.size());  // ¿Se detectaron 3 tokens?
+
+    assertEquals(TokenType.KEYWORD, tokens.get(0).getType()); // Tipo correcto
+    assertEquals("defun", tokens.get(0).getValue()); // Valor correcto
+
+    assertEquals(TokenType.KEYWORD, tokens.get(1).getType());
+    assertEquals("cond", tokens.get(1).getValue());
+
+    assertEquals(TokenType.KEYWORD, tokens.get(2).getType());
+    assertEquals("setq", tokens.get(2).getValue());
+}
 
 }
