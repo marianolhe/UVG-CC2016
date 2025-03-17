85c474f86a4606d0fd5fbb5313f3d73fc20bffb7
anaxmarcela <marcelaoe55@gmail.com>
2025-03-17 11:21:01 -0600

    javadoc tokentype

--- projectlisp/src/main/java/uvg/TokenType.java
+++ projectlisp/src/main/java/uvg/TokenType.java
@@ -1,5 +1,13 @@
 package uvg;
 
+/**
+ * Enum que define los diferentes tipos de tokens que se pueden identificar durante
+ * el proceso de tokenización.
+ * 
+ * Cada valor representa una categoría de token en el análisis léxico, como palabras clave, 
+ * operadores, números, puntuación, etc.
+*/
+
 public enum TokenType {
     KEYWORD, OPERATOR, PUNCTUATION, NUMBER, WHITESPACE, UNKNOWN, STRING, IDENTIFIER, COMMENT
 }
