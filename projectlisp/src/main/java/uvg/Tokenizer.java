50dd696fb1243816f1cedd4ec08010a6cc36aa89
anaxmarcela <marcelaoe55@gmail.com>
2025-03-17 11:18:56 -0600

    javadoc tokenizer

--- projectlisp/src/main/java/uvg/Tokenizer.java
+++ projectlisp/src/main/java/uvg/Tokenizer.java
@@ -5,17 +5,33 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * La clase Tokenizer divide una cadena de texto en tokens significativos, como palabras clave,
+ * operadores, identificadores, números, puntuación y espacios en blanco. 
+ */
+
 public class Tokenizer {
     private String input;
     private int currentPosition;
     private List<Token> tokens;
 
+    /**
+     * Constructor que inicializa el Tokenizer con la cadena de entrada.
+     * 
+     * @param input La cadena de texto a tokenizar.
+     */    
     public Tokenizer(String input) {
         this.input = input;
         this.currentPosition = 0;
         this.tokens = new ArrayList<>();
     }
 
+    /**
+     * Realiza la tokenización de la cadena, eliminando comentarios y dividiendo
+     * el texto en tokens según los patrones definidos.
+     * 
+     * @return Lista de tokens identificados en la cadena.
+     */
     public List<Token> Tokenize() {
         tokens.clear();
         Token token;
@@ -33,11 +49,22 @@ public class Tokenizer {
         return tokens;
     }
 
+    /**
+     * Elimina los comentarios de la cadena de entrada (comentarios estilo LISP).
+     * 
+     * @param code Código de entrada con posibles comentarios.
+     * @return La cadena sin comentarios.
+     */
     private String removeComments(String code) {
         // Eliminar comentarios de línea estilo ;
         return code.replaceAll(";.*", "");
     }
 
+    /**
+     * Obtiene el siguiente token de la cadena, basándose en patrones predefinidos.
+     * 
+     * @return El siguiente token o null si se llega al final de la cadena.
+     */    
     private Token nextToken() {
         if (currentPosition >= input.length()) {
             return null;
